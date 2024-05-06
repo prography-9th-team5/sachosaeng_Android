e4932ab94be2e3de38123f246e6257f70817f3e6
soyeonLee <109227292+soyeonLee126@users.noreply.github.com>
2024-05-06 14:28:14 +0900

    hilt 설정

--- app/src/main/java/com/example/sachosaeng/App.kt
+++ app/src/main/java/com/example/sachosaeng/App.kt
@@ -2,8 +2,10 @@ package com.example.sachosaeng
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
 
 
+@HiltAndroidApp
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,17 @@
+import org.jetbrains.kotlin.fir.declarations.builder.buildScript
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
     id("com.android.library") version "8.2.2" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.44")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+    }
 }
