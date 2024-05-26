039fc8ac745891429349214f2d465082b3d74068
soyeonLee <109227292+soyeonLee126@users.noreply.github.com>
2024-05-26 16:25:30 +0900

    firebase sdk 설정 수정

--- app/src/main/java/com/example/sachosaeng/App.kt
+++ app/src/main/java/com/example/sachosaeng/App.kt
@@ -1,6 +1,7 @@
 package com.example.sachosaeng
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
@@ -9,6 +10,7 @@ import dagger.hilt.android.HiltAndroidApp
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
+        FirebaseApp.initializeApp(this)
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
     }
 }
