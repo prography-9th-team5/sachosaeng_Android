7e8d38c71f2fcec6c67478a01602708cadce1b97
soyeonLee <109227292+soyeonLee126@users.noreply.github.com>
2024-07-31 22:37:01 +0900

    withdraw screen 추가

---
+++ feature/src/main/java/com/example/sachosaeng/feature/mypage/withdraw/WIthdrawUiState.kt
@@ -0,0 +1,17 @@
+package com.example.sachosaeng.feature.mypage.withdraw
+
+data class WIthdrawUiState(
+    val userName: String = "",
+    val reasonForWithdrawList: List<Int> = emptyList(),
+    val reasonForWithdrawDetail: String = "",
+    val reasonForWithdrawDetailFieldIsOpened: Boolean = false,
+    val selectedReason: ReasonForWithdraw = ReasonForWithdraw.NONE,
+)
+
+enum class ReasonForWithdraw {
+    DISSATISFACTION,
+    LACK_OF_CONTENT,
+    NO_LONGER_USE,
+    ETC,
+    NONE
+}
