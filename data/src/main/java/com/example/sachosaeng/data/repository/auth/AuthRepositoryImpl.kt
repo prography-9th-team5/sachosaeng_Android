de3c6d1085af0805d1015c0e7212446c08506cb4
soyeonLee <109227292+soyeonLee126@users.noreply.github.com>
2024-08-24 15:44:14 +0900

    call adapter 수정

--- data/src/main/java/com/example/sachosaeng/data/repository/auth/AuthRepositoryImpl.kt
+++ data/src/main/java/com/example/sachosaeng/data/repository/auth/AuthRepositoryImpl.kt
@@ -1,13 +1,14 @@
 package com.example.sachosaeng.data.repository.auth
 
-import com.example.sachosaeng.core.domain.model.User
 import com.example.sachosaeng.data.api.AuthService
+import com.example.sachosaeng.data.datasource.datastore.AuthDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
-    private val authService: AuthService
+    private val authService: AuthService,
+    private val authLocalDataSource: AuthDataStore
 ) : AuthRepository {
     override fun login(id: String, password: String) {
         TODO("Not yet implemented")
