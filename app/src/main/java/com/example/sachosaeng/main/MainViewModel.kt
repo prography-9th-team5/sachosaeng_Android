b593a2f337b9039baa69116c3288b5af4e1292c0
torder-raven <109227292+soyeonLee126@users.noreply.github.com>
2024-09-05 17:35:20 +0900

    Update MainViewModel.kt

--- app/src/main/java/com/example/sachosaeng/main/MainViewModel.kt
+++ app/src/main/java/com/example/sachosaeng/main/MainViewModel.kt
@@ -1,10 +1,6 @@
 package com.example.sachosaeng.main;
 
-import android.content.ContentValues
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.kakao.sdk.user.UserApiClient
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor() : ViewModel() {
