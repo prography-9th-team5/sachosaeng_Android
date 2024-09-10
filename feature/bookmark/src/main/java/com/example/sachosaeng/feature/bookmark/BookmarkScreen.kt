da95cfa827a94b954a907dfe5adf16da7bdc23f7
soyeonLee <109227292+soyeonLee126@users.noreply.github.com>
2024-09-11 02:38:09 +0900

    이미지 추가

--- feature/bookmark/src/main/java/com/example/sachosaeng/feature/bookmark/BookmarkScreen.kt
+++ feature/bookmark/src/main/java/com/example/sachosaeng/feature/bookmark/BookmarkScreen.kt
@@ -2,14 +2,19 @@ package com.example.sachosaeng.feature.bookmark
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -17,6 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import com.example.sachosaeng.core.model.Category
 import com.example.sachosaeng.core.ui.R
 import com.example.sachosaeng.core.ui.UserType
@@ -73,12 +79,12 @@ fun VoteColumnByCategory(
     categories: List<Category>,
     modifier: Modifier = Modifier
 ) {
-    Column(
+    LazyRow(
         modifier = modifier
             .padding(10.dp)
     ) {
-        categories.forEach {
-            CategoryCard(category = it)
+        items(categories.size) {
+            CategoryCard(category = categories[it])
         }
     }
 }
@@ -97,12 +103,24 @@ fun CategoryCard(
         modifier = modifier
             .padding(10.dp)
     ) {
-        Text(
+        Row(
             modifier = modifier.padding(10.dp),
-            text = category.name,
-            fontSize = 15.sp,
-            fontWeight = FontWeight.W600
-        )
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = category.name,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.W600
+            )
+            category.imageUrl?.let {
+                AsyncImage(
+                    alignment = Alignment.CenterEnd,
+                    contentDescription = "", model = it,
+                    modifier = Modifier
+                        .size(18.dp)
+                )
+            }
+        }
     }
 }
 
