4ec1fda82f379b5a2bfac81f4ae8c2a0670d6b85
torder-raven <109227292+soyeonLee126@users.noreply.github.com>
2024-09-12 18:36:25 +0900

    conflict 수정

---
+++ core/util/src/main/java/com/example/sachosaeng/core/util/constant/ColorConstant.kt
@@ -0,0 +1,5 @@
+package com.example.sachosaeng.core.util.constant
+
+object ColorConstant {
+    const val GS_BLACK_CODE = "#000000"
+}
--- data/src/main/java/com/example/sachosaeng/data/api/CategoryService.kt
+++ data/src/main/java/com/example/sachosaeng/data/api/CategoryService.kt
@@ -2,7 +2,7 @@ package com.example.sachosaeng.data.api
 
 import com.example.sachosaeng.data.model.BaseResponse
 import com.example.sachosaeng.data.model.category.AllCategoryIconResponse
-import com.example.sachosaeng.data.model.category.CategoryResponse
+import com.example.sachosaeng.data.model.category.CategoriesResponse
 import com.example.sachosaeng.data.model.category.SetCategoryRequest
 import com.example.sachosaeng.data.remote.util.ApiResult
 import retrofit2.http.Body
@@ -11,11 +11,11 @@ import retrofit2.http.PUT
 
 interface CategoryService {
     @GET("/api/v1/categories")
-    suspend fun getAllCategoryList(): ApiResult<BaseResponse<List<CategoryResponse>>>
+    suspend fun getAllCategoryList(): ApiResult<BaseResponse<CategoriesResponse>>
     @GET("/api/v1/categories/icon-data/all")
     suspend fun getAllCategoryIcon(): ApiResult<BaseResponse<AllCategoryIconResponse>>
     @GET("/api/v1/my-categories")
-    suspend fun getMyCategoryList(): ApiResult<BaseResponse<List<CategoryResponse>>>
+    suspend fun getMyCategoryList(): ApiResult<BaseResponse<CategoriesResponse>>
     @PUT("/api/v1/my-categories")
     suspend fun setMyCategoryList(
         @Body categoryIds: SetCategoryRequest
