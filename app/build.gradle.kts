167449fe3a0648e4f5e6f9394795df1142fa31bf
torder-raven <109227292+soyeonLee126@users.noreply.github.com>
2024-09-22 23:38:50 +0900

    버전수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.example.sachosaeng"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10001
-        versionName = "1.0.1"
+        versionCode = 10002
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
