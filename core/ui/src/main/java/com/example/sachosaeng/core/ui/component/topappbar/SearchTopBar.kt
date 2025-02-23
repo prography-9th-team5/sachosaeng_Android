27aa91c6206f9a823039da0ae41aa1c6d916f2cc
soyeonLee <109227292+soyeonLee126@users.noreply.github.com>
2025-02-23 11:55:03 +0900

    최적화

--- core/ui/src/main/java/com/example/sachosaeng/core/ui/component/topappbar/SearchTopBar.kt
+++ core/ui/src/main/java/com/example/sachosaeng/core/ui/component/topappbar/SearchTopBar.kt
@@ -16,11 +16,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
@@ -32,75 +30,66 @@ import com.sachosaeng.app.core.ui.R
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
-
 @Composable
 fun SearchTopBar(
     modifier: Modifier = Modifier,
     placeholder: String = "",
     navigateToBackStack: () -> Unit
 ) {
-    val scope = rememberCoroutineScope()
     var maxWidth = remember { 0 }
     val offsetX = remember { Animatable(100f) }
     val alphaAnim = remember { Animatable(0f) }
     val widthAnim = remember { Animatable(0f) }
+    val animationValue = 800
 
-    LaunchedEffect(maxWidth > 0) {
-        scope.launch {
-            offsetX.animateTo(
-                targetValue = 0f,
-                animationSpec = tween(
-                    durationMillis = 800,
-                    easing = FastOutSlowInEasing
-                )
-            )
-        }
-        scope.launch {
-            widthAnim.animateTo(
-                targetValue = 1f,
-                animationSpec = tween(
-                    durationMillis = 800,
-                    easing = FastOutSlowInEasing
-                )
-            )
-        }
-        scope.launch {
-            alphaAnim.animateTo(
-                targetValue = 1f,
-                animationSpec = tween(
-                    durationMillis = 800,
-                    easing = LinearEasing
-                )
-            )
+    LaunchedEffect(maxWidth) {
+        if (maxWidth > 0) {
+            listOf(
+                launch {
+                    offsetX.animateTo(
+                        targetValue = 0f,
+                        animationSpec = tween(animationValue, easing = FastOutSlowInEasing)
+                    )
+                },
+                launch {
+                    widthAnim.animateTo(
+                        targetValue = 1f,
+                        animationSpec = tween(animationValue, easing = FastOutSlowInEasing)
+                    )
+                },
+                launch {
+                    alphaAnim.animateTo(
+                        targetValue = 1f,
+                        animationSpec = tween(animationValue, easing = LinearEasing)
+                    )
+                }
+            ).forEach { it.join() }
         }
     }
 
     Row(
         modifier = modifier
-            .onSizeChanged {
-                maxWidth = it.width
-            }
+            .onSizeChanged { maxWidth = it.width }
             .fillMaxWidth()
             .padding(20.dp),
         horizontalArrangement = Arrangement.spacedBy(20.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
-            modifier = modifier
+            modifier = Modifier
                 .offset { IntOffset(offsetX.value.roundToInt(), 0) }
-                .clickable { navigateToBackStack() }
-                .background(Color.Transparent),
+                .clickable { navigateToBackStack() },
             painter = painterResource(id = R.drawable.ic_go_back),
             contentDescription = null
         )
         Row(
-            modifier = modifier
+            modifier = Modifier
                 .fillMaxWidth()
                 .background(White, RoundedCornerShape(20.dp)),
             verticalAlignment = Alignment.CenterVertically
         ) {
             SachosaengSearchTextField(
-                modifier = modifier
+                modifier = Modifier
                     .fillMaxWidth(widthAnim.value)
                     .alpha(alphaAnim.value),
                 placeholder = placeholder,
